30fe4a876b33bd9f2fe3dbf990c4ccf5901079a8
zhangrenyang <zhang_renyang@126.com>
2020-09-27 10:42:36 +0800

    fix regexp

--- extension.js
+++ extension.js
@@ -3,7 +3,7 @@ function activate(context) {
 	let disposable = vscode.commands.registerCommand('vscode-plugin-remove-comments.removeComments', function () {
 		vscode.window.activeTextEditor.edit(editBuilder => {
 			let text = vscode.window.activeTextEditor.document.getText();
-			text=text.replace(/(\/\*([\w\W]+?)\*\/)|(\/\/.+?(?=["$\r\n]))/gm,'').replace(/(^\s*(?=\r?$)\n)/gm,'');
+			text=text.replace(/((\/\*([\w\W]+?)\*\/)|(\/\/(.(?!"\)))+)|(^\s*(?=\r?$)\n))/gm,'').replace(/(^\s*(?=\r?$)\n)/gm,'').replace(/\\n\\n\?/gm,'');
 			const end = new vscode.Position(vscode.window.activeTextEditor.document.lineCount + 1, 0);
 			editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), text);
 			vscode.window.activeTextEditor;
